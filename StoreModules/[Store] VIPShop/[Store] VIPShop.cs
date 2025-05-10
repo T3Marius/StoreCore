96695df03d9b3d341e5d8eb63677679ec7bf4d31
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-05-10 16:49:15 +0300

    added: vip shop

---
+++ StoreModules/[Store] VIPShop/[Store] VIPShop.cs
@@ -0,0 +1,77 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.Logging;
+using StoreAPI;
+
+namespace StoreCore;
+
+public class VIPShop : BasePlugin
+{
+    public override string ModuleAuthor => "T3Marius";
+    public override string ModuleName => "[Store] VIPShop";
+    public override string ModuleVersion => "1.0.0";
+    public IStoreAPI? StoreApi;
+    public PluginConfig Config { get; set; } = new PluginConfig();
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        StoreApi = IStoreAPI.Capability.Get() ?? throw new Exception("StoreApi not found");
+        Config = StoreApi.GetModuleConfig<PluginConfig>("VIPShop");
+
+        foreach (var kvp in Config.Vips)
+        {
+            var vip = kvp.Value;
+
+            StoreApi.RegisterItem(
+                vip.Id,
+                vip.Name,
+                Config.Category,
+                vip.Type,
+                vip.Price,
+                vip.Description,
+                isEquipable: false,
+                isSellable: false
+            );
+        }
+
+        StoreApi.OnPlayerPurchaseItem += OnPlayerPurchaseItem;
+    }
+    public void OnPlayerPurchaseItem(CCSPlayerController player, Dictionary<string, string> item)
+    {
+        foreach (var vip in Config.Vips.Values)
+        {
+            if (item["uniqueid"] == vip.Id)
+            {
+                string command = vip.Command.Replace("{steamid}", player.SteamID.ToString());
+                Server.ExecuteCommand(command);
+                Logger.LogInformation("Executed command {command} for {steamid}", command, player.SteamID);
+            }
+        }
+    }
+}
+public class PluginConfig
+{
+    public string Category { get; set; } = "VIP Shop";
+    public Dictionary<string, Vip_Item> Vips { get; set; } = new Dictionary<string, Vip_Item>()
+    {
+        {
+            "1", new Vip_Item
+            {
+                Id = "vip_2_days",
+                Name = "Vip [2 Days]",
+                Command = "css_vip_adduser {steamid} VIP 172800",
+                Type = "vip",
+                Description = "Gives you VIP for 2 days",
+                Price = 5000,
+            }
+        }
+    };
+}
+public class Vip_Item
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Command { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Price { get; set; } = 0;
+}
